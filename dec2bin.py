b7f4a3f4afc0364d9bd9d4adaf170977a26c67db
DavidBaGi <90776565+DavidBaGi@users.noreply.github.com>
2021-09-29 17:17:39 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -1,4 +1,4 @@
-#Hola, soy david y estoy modificando esto xd
+#Hola, soy david y estoy modificando esto jajajaja equisde omegalul
 # -----------# ------------------------------------------------------
 # Convierte un número decimal positivo a binario usando un 
 #   determinado número de bits.
