5e64fbbbddcb5f675b33f7179314d4883190de4e
DavidBaGi <90776565+DavidBaGi@users.noreply.github.com>
2021-09-29 16:31:33 +0200

    Update dec2bin.py

--- dec2bin.py
+++ dec2bin.py
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     # Muestra por pantalla el resultado.
     # Para imprimir un entero es necesario convertirlo a string con str()
     print("El numero " + str(numero_decimal) + " es " + numero_binario + " en binario con " + str(numero_bits) + " bits.")
-     #Hola, soy david y estoy modificando esto xd
+    #Hola, soy david y estoy modificando esto xd
